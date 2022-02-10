228114860ccad55208d65df7bc489691809514ad
Konstantin Kotsarev <kotsaarev@gmail.com>
2022-02-10 17:41:47 +0700

    Lecture 7: ViewModifier Animation (Memorize)

--- Memorize/Memorize/EmojiMemoryGame.swift
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -13,7 +13,7 @@ class EmojiMemoryGame: ObservableObject {
     private static let emojis = ["🚲", "🚂", "🚁", "🚜", "🚕", "🏎", "🚑", "🚓", "🚒", "✈️", "🚀", "⛵️", "🛸", "🛶", "🚌", "🏍", "🛺", "🚠", "🛵", "🚗", "🚚", "🚇", "🛻", "🚝"]
     
     private static func createMemoryGame() -> MemoryGame<String> {
-        MemoryGame<String>(numberOfPairsOfCards: 6) { pairIndex in 
+        MemoryGame<String>(numberOfPairsOfCards: 8) { pairIndex in 
             emojis[pairIndex]
         }
     }
