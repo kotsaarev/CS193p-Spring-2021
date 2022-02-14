48a29260fd16ca05d335241890a7a66cb972a447
Konstantin Kotsarev <kotsaarev@gmail.com>
2022-02-14 14:58:00 +0700

    Lecture 9: EmojiArt Drag/Drop (EmojiArt)

---
+++ EmojiArt/EmojiArt/EmojiArtModel.Background.swift
@@ -0,0 +1,30 @@
+//
+//  EmojiArtModel.Background.swift
+//  EmojiArt
+//
+//  Created by Konstantin Kotsarev on 14.02.2022.
+//
+
+import Foundation
+
+extension EmojiArtModel {
+    enum Background {
+        case blank
+        case url(URL)
+        case imageData(Data)
+        
+        var url: URL? {
+            switch self {
+            case .url(let url): return url
+            default: return nil
+            }
+        }
+        
+        var imageData: Data? {
+            switch self {
+            case .imageData(let data): return data
+            default: return nil
+            }
+        }
+    }
+}
