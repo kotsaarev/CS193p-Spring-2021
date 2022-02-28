46ebb6de74cfb000d4c63119717df9396960baab
Konstantin Kotsarev <kotsaarev@gmail.com>
2022-02-28 16:17:09 +0700

    Lecture 12: Binding Sheet Navigation EditMode (EmojiArt)

---
+++ EmojiArt/EmojiArt/PaletteEditor.swift
@@ -0,0 +1,73 @@
+//
+//  PaletteEditor.swift
+//  EmojiArt
+//
+//  Created by Konstantin Kotsarev on 28.02.2022.
+//
+
+import SwiftUI
+
+struct PaletteEditor: View {
+    @Binding var palette: Palette
+    
+    var body: some View {
+        Form {
+            nameSection
+            addEmojisSection
+            removeEmojiSection
+        }
+        .navigationTitle("Edit \(palette.name)")
+        .frame(minWidth: 300, minHeight: 350)
+    }
+    
+    var nameSection: some View {
+        Section(header: Text("Name")) {
+            TextField("Name", text: $palette.name)
+        }
+    }
+    
+    @State private var emojisToAdd = ""
+    
+    var addEmojisSection: some View {
+        Section(header: Text("Add Emojis")) {
+            TextField("", text: $emojisToAdd)
+                .onChange(of: emojisToAdd) { emojis in
+                    addEmoji(emojis)
+                }
+        }
+    }
+    
+    func addEmoji(_ emojis: String) {
+        withAnimation {
+            palette.emojis = (emojis + palette.emojis)
+                .filter { $0.isEmoji }
+                .removingDuplicateCharacters
+        }
+    }
+    
+    var removeEmojiSection: some View {
+        Section(header: Text("Remove Emoji")) {
+            let emojis = palette.emojis.removingDuplicateCharacters.map { String($0) }
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 40))]) {
+                ForEach(emojis, id: \.self) { emoji in
+                    Text(emoji)
+                        .onTapGesture {
+                            withAnimation {
+                                palette.emojis.removeAll(where: { String($0) == emoji })
+                            }
+                        }
+                }
+            }
+            .font(.system(size: 40))
+        }
+    }
+}
+
+struct PaletteEditor_Previews: PreviewProvider {
+    static var previews: some View {
+        PaletteEditor(palette: .constant(PaletteStore(named: "Preview").palette(at: 4)))
+            .previewLayout(.fixed(width: /*@START_MENU_TOKEN@*/300.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/350.0/*@END_MENU_TOKEN@*/))
+        PaletteEditor(palette: .constant(PaletteStore(named: "Preview").palette(at: 2)))
+            .previewLayout(.fixed(width: /*@START_MENU_TOKEN@*/300.0/*@END_MENU_TOKEN@*/, height: 600.0))
+    }
+}
