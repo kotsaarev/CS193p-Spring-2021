93eab02f77fa1c40a7da904c82a6cb0c381a99dd
Konstantin Kotsarev <kotsaarev@gmail.com>
2022-03-03 17:23:27 +0700

    Lecture 16: Multiplatform (macOS) (EmojiArt)

---
+++ EmojiArt/Shared/EmojiArtApp.swift
@@ -0,0 +1,20 @@
+//
+//  EmojiArtApp.swift
+//  Shared
+//
+//  Created by Konstantin Kotsarev on 03.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    @StateObject var paletteStore = PaletteStore(named: "Default")
+    
+    var body: some Scene {
+        DocumentGroup(newDocument: { EmojiArtDocument() }) { config in
+            EmojiArtDocumentView(document: config.document)
+                .environmentObject(paletteStore)
+        }
+    }
+}
---
+++ EmojiArt/iOS/iOS.swift
@@ -0,0 +1,58 @@
+//
+//  iOS.swift
+//  EmojiArt
+//
+//  Created by Konstantin Kotsarev on 03.03.2022.
+//
+
+import SwiftUI
+
+extension UIImage {
+    var imageData: Data? { jpegData(compressionQuality: 1.0) }
+}
+
+struct Pasteboard {
+    static var imageData: Data? {
+        UIPasteboard.general.image?.imageData
+    }
+    static var imageURL: URL? {
+        UIPasteboard.general.url?.imageURL
+    }
+}
+
+extension View {
+    func paletteControlButtonStyle() -> some View {
+        self
+    }
+    
+    func popoverPadding() -> some View {
+        self
+    }
+    
+    @ViewBuilder
+    func wrappedInNavigationViewToMakeDismissable(_ dismiss: (() -> Void)?) -> some View {
+        if UIDevice.current.userInterfaceIdiom != .pad, let dismiss = dismiss {
+            NavigationView {
+                self
+                    .navigationBarTitleDisplayMode(.inline)
+                    .dismissable(dismiss)
+            }
+            .navigationViewStyle(StackNavigationViewStyle())
+        } else {
+            self
+        }
+    }
+    
+    @ViewBuilder
+    func dismissable(_ dismiss: (() -> Void)?) -> some View {
+        if UIDevice.current.userInterfaceIdiom != .pad, let dismiss = dismiss {
+            self.toolbar {
+                ToolbarItem(placement: .cancellationAction) {
+                    Button("Close") { dismiss() }
+                }
+            }
+        } else {
+            self
+        }
+    }
+}
